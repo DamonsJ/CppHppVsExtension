80e6b6cb352f2249426c2df161614348144bbe5c
DamonsJ <shlkl99@163.com>
2023-12-14 22:53:56 +0800

    pass test

---
+++ src/extension.ts
@@ -0,0 +1,80 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as path from 'path';
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	let fs = require('fs');
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "cpphpp" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('cpphpp.CppHpp', (uri: vscode.Uri) => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		console.log(uri.fsPath);
+		vscode.window.showInputBox({
+			placeHolder: "Please enter a file name",
+		}).then((fileName) => {
+			if (!fileName) return;
+			try {
+				let cppfile = path.join(uri.fsPath, fileName + '.cpp');
+				let hppfile = path.join(uri.fsPath, fileName + '.hpp');
+				console.log(cppfile);
+				console.log(hppfile);
+				if (!fs.existsSync(cppfile)) {
+					fs.createWriteStream(cppfile).close();
+				}
+
+				if (!fs.existsSync(hppfile)) {
+					fs.createWriteStream(hppfile).close();
+				}
+				var uppername = fileName.toUpperCase()
+				const cpp_content = '#include \"' + fileName + '.hpp\"\n';
+				let hpp_content = `#ifndef __${uppername}_HPP__\n#define __${uppername}_HPP__\n\n#include <iostream>\n\n`;
+				const comment = `\/**\n* @brief ${fileName}\n*\n*\/ \n\n`;
+				const class_str = `class ${fileName}\n{\npublic:\n     ${fileName}();\n     ~${fileName}();\n};\n\n#endif\n`;
+				hpp_content = hpp_content + comment + class_str;
+
+				const writeData = Buffer.from(cpp_content, 'utf8');
+				vscode.workspace.fs.writeFile(vscode.Uri.file(cppfile), writeData);
+
+				const hwriteData = Buffer.from(hpp_content, 'utf8');
+				vscode.workspace.fs.writeFile(vscode.Uri.file(hppfile), hwriteData);
+
+				setTimeout(() => { //tiny delay
+					let openPath = fs.lstatSync(cppfile).isFile()
+					if (!openPath) return;
+					vscode.workspace.openTextDocument(openPath)
+						.then((editor) => {
+							if (!editor) return;
+							vscode.window.showTextDocument(editor);
+						});
+
+					openPath = fs.lstatSync(hppfile).isFile()
+					if (!openPath) return;
+					vscode.workspace.openTextDocument(openPath)
+						.then((editor) => {
+							if (!editor) return;
+							vscode.window.showTextDocument(editor);
+						});
+
+				}, 50);
+			} catch (error) {
+				console.log(error);
+				vscode.window.showErrorMessage("Somthing went wrong! Please report on GitHub");
+			}
+		});
+		vscode.window.showInformationMessage('CppHpp Loaded!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() { }
---
+++ test.hpp
@@ -0,0 +1,17 @@
+#ifndef __TEST_HPP__
+#define __TEST_HPP__
+
+#include <iostream>
+
+/**
+ * @brief Test
+ *
+ */
+class Test
+{
+public:
+    Test();
+    ~Test();
+};
+
+#endif
